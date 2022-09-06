df6d75222c60a93f6c1bc1d4816e38e2ab621298
godseun_mac <alcy1024@gmail.com>
2022-09-06 15:36:52 +0900

    clear && new init project

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import {
   SwaggerDocumentOptions,
 } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+// import { TodoModule } from './todo/todo.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
